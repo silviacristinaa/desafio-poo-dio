6bba2323c8591198438d2e1783fa806b595dc5c7
silviacristinaa <silviacristinaalexandre1@gmail.com>
2022-10-28 10:40:44 -0300

    First Commit

---
+++ src/main/java/com/github/silviacristinaa/desafiopoodio/DesafioPooDioApplication.java
@@ -0,0 +1,13 @@
+package com.github.silviacristinaa.desafiopoodio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPooDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPooDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/silviacristinaa/desafiopoodio/DesafioPooDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.silviacristinaa.desafiopoodio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPooDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
